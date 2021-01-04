1b8a93680f70b8ecc1136a896119f058b6ef5c14
ViviRofiah <44234734+ViviRofiah@users.noreply.github.com>
2021-01-04 23:42:21 +0700

    Update kmodes.java

--- others/kmodes.java
+++ others/kmodes.java
@@ -41,7 +41,28 @@ public class kmodes {
 
     // parameter
     // private int m_numClusters;
+    public static List<List<String>> randomCluster(int k, List<List<String>> result) {
+        System.out.println(result);
+        List<List<String>> df = new ArrayList<>();
 
+        List<List<String>> random_k = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            // buat random
+            Random random = new Random();
+            int indeks = random.nextInt(result.size());
+            // System.out.println(indeks);
+            List<String> cluster = result.get(indeks);
+            random_k.add(result.get(indeks));
+            // System.out.println("==============");
+            // System.out.println(random_k);
+        }
+        System.out.println("================================================");
+        System.out.println("hasil random = " + random_k);
+
+        return df;
+
+    }
+    
     public k_cluster (double instance, string[] k) {
         
     }

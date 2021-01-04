5c7c8dcf01dd15a9368ef52557f1a0c7f67086fd
nadiramei <46215924+nadiramei@users.noreply.github.com>
2021-01-04 20:12:27 +0700

    Update kModes.java

--- kModes.java
+++ kModes.java
@@ -11,6 +11,7 @@ public class kModes {
     private static List<String> cluster = new ArrayList<>();
     static int indeks;
     static List<Integer> ind = new ArrayList<>();
+    static List<List<String>> cluster1 = new ArrayList<>();
 
     private static List<Integer> nilaiK (int k){
         for (int i = 0; i < k; i ++){
@@ -19,36 +20,62 @@ public class kModes {
             cluster = result.get(indeks);
             System.out.println("indeks: " + indeks + "\n" + cluster + "\n");
             ind.add(indeks);
+            cluster1.add(cluster);
+            //System.out.println("cluster: " + cluster1);
         }
         return ind;
     } 
 
+    static int in;
+    static List<String> instansi = new ArrayList<>();
+    
     private static int hammingDist (){
-        for (int i = 0; i < result.size(); i++){
-            for (int j = 0; j < ind.size(); j++){
-                List<String> instansi = result.get(i);
-                System.out.println(instansi);
-                //for (int j = 0; j < instansi.size(); j++){
-                //    elemen = instansi.get(j);
-                    //System.out.println(elemen + "-");
-                // }
-                System.out.println(cluster);
-                
-                ham = 0;
-                for (int l = 0; l < instansi.size(); l++){
-                    //elemen = instansi.get(j);
-                    
-                    if(!instansi.get(j).equals(cluster.get(j))){
-                        ham++;
-                    }
-                    
-                }
-                System.out.println(ham);
-                System.out.println("-----");
+        for (int i = 0; i < ind.size(); i ++){
+            in = ind.get(i);
+            System.out.println(in + "->" + cluster);
+
+            for (int j = 0; j < result.size(); j++){
+                instansi = result.get(i);
+                //System.out.println(instansi);
             }
             
         }
-        return ham;
+
+        /*
+        for (int i = 0; i < result.size(); i++){
+            List<String> instansi = result.get(i);
+            System.out.println(instansi);
+            System.out.println(cluster);
+            for (int j = 0; j < ind.size(); j++){
+                in = ind.get(j);
+               // System.out.println(in);
+            }
+            
+            List<String> instansi = result.get(i);
+            System.out.println(instansi);
+            System.out.println(cluster);
+            
+            ham = 0;
+            for (int l = 0; l < instansi.size(); l++){
+                //elemen = instansi.get(j);
+                
+                if(!instansi.get(j).equals(cluster.get(j))){
+                    ham++;
+                }
+                
+            }
+            System.out.println(ham);
+            System.out.println("-----");
+            */
+        return 2;
+    }
+
+    private static int assignCentroid(){
+        return 1;
+    }
+
+    private static int countModes() {
+        return 2;
     }
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new FileReader("./golfweather.csv"))) {
